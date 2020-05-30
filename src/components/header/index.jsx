7af171f8dc84e7be0ef0c044256a1d1d1405e150
Dmitrii Pisarenko <pisarenko.d.y@gmail.com>
2020-05-30 21:45:24 +0600

    add side choice button

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -1,10 +1,22 @@
 import React from 'react';
 import styles from './header.module.css';
+import classNames from 'classnames';
+import circle from '../../assets/images/circle.png'
+import cross from '../../assets/images/cross.png'
 
 export default function Header() {
   return (
     <div className={styles.container}>
-      
+      <div className={styles.side} >
+        <img className={styles.image} src={circle} alt='circle' />
+      </div>
+
+      <div className={classNames(styles.switchBtn, styles
+      .switchOn)} />
+
+      <div className={styles.side} >
+        <img className={styles.image} src={cross} alt='cross' />
+      </div>
     </div>
   )
 
