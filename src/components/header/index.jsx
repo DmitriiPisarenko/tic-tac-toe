f93d9296c09dcd06fd9e9e9654b5290542b360a5
Dmitrii Pisarenko <pisarenko.d.y@gmail.com>
2020-05-31 12:06:59 +0600

    add score and changed side button

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -1,22 +1,22 @@
 import React from 'react';
 import styles from './header.module.css';
-import classNames from 'classnames';
 import circle from '../../assets/images/circle.png'
 import cross from '../../assets/images/cross.png'
 
 export default function Header() {
   return (
     <div className={styles.container}>
-      <div className={styles.side} >
+      <button className={styles.side} >
         <img className={styles.image} src={circle} alt='circle' />
-      </div>
+      </button>
 
-      <div className={classNames(styles.switchBtn, styles
-      .switchOn)} />
+      <div className={styles.score} />
+      <p>-</p>
+      <div className={styles.score} />
 
-      <div className={styles.side} >
+      <button className={styles.side} >
         <img className={styles.image} src={cross} alt='cross' />
-      </div>
+      </button>
     </div>
   )
 
