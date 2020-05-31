205c194a502e54446dde817218a88f92f0f86e0a
Dmitrii Pisarenko <pisarenko.d.y@gmail.com>
2020-05-31 18:56:18 +0600

    add field generation, restart game, cell click handler

--- src/components/host/index.jsx
+++ src/components/host/index.jsx
@@ -1,14 +1,40 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import styles from './host.module.css';
 import Header from '../header';
 import Field from '../field';
+import { MARK_X, MAP_STATE_CHANGE } from '../../constants';
 
 export default function Host() {
+  const [mark, setMark] = useState(MARK_X);
+  const [field, updateField] = useState([]);
+  const [beforeStart, setBeforeStart] = useState(true);
+  useEffect(() => {
+    if (!beforeStart) {
+      return;
+    }
+    const newField = new Array(3).fill(undefined).map(() => new Array(3).fill(undefined));
+    updateField(newField);
+  }, [beforeStart]);
+
+  function onRestartClick() {
+    setBeforeStart(true);
+  }
+
+  function onCellClick(row, col) {
+    if (field[row][col] !== undefined) {
+      return;
+    }
+    const newField = field.slice();
+    newField[row][col] = mark;
+    updateField(newField);
+    setMark(MAP_STATE_CHANGE[mark]);
+    setBeforeStart(false);
+  }
+
   return (
     <div className={styles.container}>
       <Header />
-      <Field />
+      <Field field={field} onClick={onCellClick} onRestartClick={onRestartClick} />
     </div>
   );
-
 }
